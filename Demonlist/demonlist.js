fc7c9a00c47dd89297528649b51299760f2c08ae
__Hanabi__ <101558859+GDCorona@users.noreply.github.com>
2023-08-26 00:34:05 +0700

    Add files via upload

---
+++ Demonlist/demonlist.js
@@ -0,0 +1,323 @@
+/*top1*/
+var modal1 = document.getElementById("top1Info");
+var btn1 = document.getElementById("t1");
+var span1 = document.getElementsByClassName("close1")[0];
+btn1.onclick = function(){modal1.style.display = "block";}
+span1.onclick = function(){modal1.style.display = "none";}
+/*top1*/
+var modal2 = document.getElementById("top2Info");
+var btn2 = document.getElementById("t2");
+var span2 = document.getElementsByClassName("close2")[0];
+btn2.onclick = function(){modal2.style.display = "block";}
+span2.onclick = function(){modal2.style.display = "none";}
+/*top3*/
+var modal3 = document.getElementById("top3Info");
+var btn3 = document.getElementById("t3");
+var span3 = document.getElementsByClassName("close3")[0];
+btn3.onclick = function(){modal3.style.display = "block";}
+span3.onclick = function(){modal3.style.display = "none";}
+/*top4*/
+var modal4 = document.getElementById("top4Info");
+var btn4 = document.getElementById("t4");
+var span4 = document.getElementsByClassName("close4")[0];
+btn4.onclick = function(){modal4.style.display = "block";}
+span4.onclick = function(){modal4.style.display = "none";}
+/*top5*/
+var modal5 = document.getElementById("top5Info");
+var btn5 = document.getElementById("t5");
+var span5 = document.getElementsByClassName("close5")[0];
+btn5.onclick = function(){modal5.style.display = "block";}
+span5.onclick = function(){modal5.style.display = "none";}
+/*top6*/
+var modal6 = document.getElementById("top6Info");
+var btn6 = document.getElementById("t6");
+var span6 = document.getElementsByClassName("close6")[0];
+btn6.onclick = function(){modal6.style.display = "block";}
+span6.onclick = function(){modal6.style.display = "none";}
+/*top7*/
+var modal7 = document.getElementById("top7Info");
+var btn7 = document.getElementById("t7");
+var span7 = document.getElementsByClassName("close7")[0];
+btn7.onclick = function(){modal7.style.display = "block";}
+span7.onclick = function(){modal7.style.display = "none";}
+/*top8*/
+var modal8 = document.getElementById("top8Info");
+var btn8 = document.getElementById("t8");
+var span8 = document.getElementsByClassName("close8")[0];
+btn8.onclick = function(){modal8.style.display = "block";}
+span8.onclick = function(){modal8.style.display = "none";}
+/*top9*/
+var modal9 = document.getElementById("top9Info");
+var btn9 = document.getElementById("t9");
+var span9 = document.getElementsByClassName("close9")[0];
+btn9.onclick = function(){modal9.style.display = "block";}
+span9.onclick = function(){modal9.style.display = "none";}
+/*top10*/
+var modal10 = document.getElementById("top10Info");
+var btn10 = document.getElementById("t10");
+var span10 = document.getElementsByClassName("close10")[0];
+btn10.onclick = function(){modal10.style.display = "block";}
+span10.onclick = function(){modal10.style.display = "none";}
+/*top11*/
+var modal11 = document.getElementById("top11Info");
+var btn11 = document.getElementById("t11");
+var span11 = document.getElementsByClassName("close11")[0];
+btn11.onclick = function(){modal11.style.display = "block";}
+span11.onclick = function(){modal11.style.display = "none";}
+/*top12*/
+var modal12 = document.getElementById("top12Info");
+var btn12 = document.getElementById("t12");
+var span12 = document.getElementsByClassName("close12")[0];
+btn12.onclick = function(){modal12.style.display = "block";}
+span12.onclick = function(){modal12.style.display = "none";}
+/*top13*/
+var modal13 = document.getElementById("top13Info");
+var btn13 = document.getElementById("t13");
+var span13 = document.getElementsByClassName("close13")[0];
+btn13.onclick = function(){modal13.style.display = "block";}
+span13.onclick = function(){modal13.style.display = "none";}
+/*top14*/
+var modal14 = document.getElementById("top14Info");
+var btn14 = document.getElementById("t14");
+var span14 = document.getElementsByClassName("close14")[0];
+btn14.onclick = function(){modal14.style.display = "block";}
+span14.onclick = function(){modal14.style.display = "none";}
+/*top15*/
+var modal15 = document.getElementById("top15Info");
+var btn15 = document.getElementById("t15");
+var span15 = document.getElementsByClassName("close15")[0];
+btn15.onclick = function(){modal15.style.display = "block";}
+span15.onclick = function(){modal15.style.display = "none";}
+/*top16*/
+var modal16 = document.getElementById("top16Info");
+var btn16 = document.getElementById("t16");
+var span16 = document.getElementsByClassName("close16")[0];
+btn16.onclick = function(){modal16.style.display = "block";}
+span16.onclick = function(){modal16.style.display = "none";}
+/*top17*/
+var modal17 = document.getElementById("top17Info");
+var btn17 = document.getElementById("t17");
+var span17 = document.getElementsByClassName("close17")[0];
+btn17.onclick = function(){modal17.style.display = "block";}
+span17.onclick = function(){modal17.style.display = "none";}
+/*top18*/
+var modal18 = document.getElementById("top18Info");
+var btn18 = document.getElementById("t18");
+var span18 = document.getElementsByClassName("close18")[0];
+btn18.onclick = function(){modal18.style.display = "block";}
+span18.onclick = function(){modal18.style.display = "none";}
+/*top19*/
+var modal19 = document.getElementById("top19Info");
+var btn19 = document.getElementById("t19");
+var span19 = document.getElementsByClassName("close19")[0];
+btn19.onclick = function(){modal19.style.display = "block";}
+span19.onclick = function(){modal19.style.display = "none";}
+/*top20*/
+var modal20 = document.getElementById("top20Info");
+var btn20 = document.getElementById("t20");
+var span20 = document.getElementsByClassName("close20")[0];
+btn20.onclick = function(){modal20.style.display = "block";}
+span20.onclick = function(){modal20.style.display = "none";}
+
+/*close when clicking outside the modal*/
+window.onclick = function(event) {
+	if (event.target == modal1) {modal1.style.display = "none";}
+	else if (event.target == modal2) {modal2.style.display = "none";}
+	else if (event.target == modal3) {modal3.style.display = "none";}
+	else if (event.target == modal4) {modal4.style.display = "none";}
+	else if (event.target == modal5) {modal5.style.display = "none";}
+	else if (event.target == modal6) {modal6.style.display = "none";}
+	else if (event.target == modal7) {modal7.style.display = "none";}
+	else if (event.target == modal8) {modal8.style.display = "none";}
+	else if (event.target == modal9) {modal9.style.display = "none";}
+	else if (event.target == modal10) {modal10.style.display = "none";}
+	else if (event.target == modal11) {modal11.style.display = "none";}
+	else if (event.target == modal12) {modal12.style.display = "none";}
+	else if (event.target == modal13) {modal13.style.display = "none";}
+	else if (event.target == modal14) {modal14.style.display = "none";}
+	else if (event.target == modal15) {modal15.style.display = "none";}
+	else if (event.target == modal16) {modal16.style.display = "none";}
+	else if (event.target == modal17) {modal17.style.display = "none";}
+	else if (event.target == modal18) {modal18.style.display = "none";}
+	else if (event.target == modal19) {modal19.style.display = "none";}
+	else if (event.target == modal20) {modal20.style.display = "none";}
+}
+/*shared section (for every pages)*/
+//jump to top
+let mybutton = document.getElementById("jump-to-top");
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+  if (document.body.scrollTop > 3000 || document.documentElement.scrollTop > 3000) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+function backtoTop(){
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+//change theme
+var loadBG = parseInt(localStorage.getItem("bg")); //load current theme
+if (loadBG == null) loadBG = 0;
+if(loadBG == 0){
+  document.getElementsByTagName("body")[0].style.backgroundImage = "none";
+  document.documentElement.style.setProperty('--cherryblossom', 'none');
+  document.documentElement.style.setProperty('--color1', 'white');
+  document.documentElement.style.setProperty('--color2', 'black');
+  document.documentElement.style.setProperty('--color3', 'rgb(147, 224, 255)');
+  document.documentElement.style.setProperty('--color4', 'aliceblue');
+  document.documentElement.style.setProperty('--switchcolor', '#cefaf8');
+  document.documentElement.style.setProperty('--hovercolor', 'rgb(70, 157, 228)');
+  document.getElementById("menuIcon").src = "listLight.png";
+}
+else if (loadBG == 1){
+  document.getElementsByTagName("body")[0].style.backgroundImage = "none";
+  document.documentElement.style.setProperty('--cherryblossom', 'none');
+  document.documentElement.style.setProperty('--color1', 'black');
+  document.documentElement.style.setProperty('--color2', 'white');
+  document.documentElement.style.setProperty('--color3', 'rgb(30, 62, 75)');
+  document.documentElement.style.setProperty('--color4', 'rgba(135, 167, 172, 0.925)');
+  document.documentElement.style.setProperty('--switchcolor', '#8d8d8d');
+  document.documentElement.style.setProperty('--hovercolor', 'rgb(220, 221, 243)');
+  document.getElementById("menuIcon").src = "listDark.png";
+}
+else if (loadBG == 2){
+  document.getElementsByTagName("body")[0].style.backgroundImage = "none";
+  document.documentElement.style.setProperty('--cherryblossom', 'block');
+  document.documentElement.style.setProperty('--color1', 'rgba(250, 206, 246, 0.829)');
+  document.documentElement.style.setProperty('--color2', 'rgb(223, 14, 129)');
+  document.documentElement.style.setProperty('--color3', 'rgb(238, 172, 239)');
+  document.documentElement.style.setProperty('--color4', 'rgba(255, 232, 253, 0.8)');
+  document.documentElement.style.setProperty('--switchcolor', '#fccbf5');
+  document.documentElement.style.setProperty('--hovercolor', 'rgb(185, 71, 185)');
+  document.getElementById("menuIcon").src = "listSakura.png";
+}
+var Theme_click_count = 0;
+function ChangeTheme(value){
+  if(value == 0){
+      document.getElementsByTagName("body")[0].style.backgroundImage = "none";
+      document.documentElement.style.setProperty('--cherryblossom', 'none');
+      document.documentElement.style.setProperty('--color1', 'white');
+      document.documentElement.style.setProperty('--color2', 'black');
+      document.documentElement.style.setProperty('--color3', 'rgb(147, 224, 255)');
+      document.documentElement.style.setProperty('--color4', 'aliceblue');
+      document.documentElement.style.setProperty('--switchcolor', '#cefaf8');
+      document.documentElement.style.setProperty('--hovercolor', 'rgb(70, 157, 228)');
+      document.getElementById("menuIcon").src = "listLight.png";
+  }
+  else if (value == 1){
+      document.getElementsByTagName("body")[0].style.backgroundImage = "none";
+      document.documentElement.style.setProperty('--cherryblossom', 'none');
+      document.documentElement.style.setProperty('--color1', 'black');
+      document.documentElement.style.setProperty('--color2', 'white');
+      document.documentElement.style.setProperty('--color3', 'rgb(30, 62, 75)');
+      document.documentElement.style.setProperty('--color4', 'rgba(135, 167, 172, 0.925)');
+      document.documentElement.style.setProperty('--switchcolor', '#8d8d8d');
+      document.documentElement.style.setProperty('--hovercolor', 'rgb(220, 221, 243)');
+      document.getElementById("menuIcon").src = "listDark.png";
+  }
+  else if (value == 2){
+      document.getElementsByTagName("body")[0].style.backgroundImage = "none";
+      document.documentElement.style.setProperty('--cherryblossom', 'block');
+      document.documentElement.style.setProperty('--color1', 'rgba(250, 206, 246, 0.829)');
+      document.documentElement.style.setProperty('--color2', 'rgb(223, 14, 129)');
+      document.documentElement.style.setProperty('--color3', 'rgba(238, 172, 239, 1)');
+      document.documentElement.style.setProperty('--color4', 'rgba(255, 232, 253, 0.8)');
+      document.documentElement.style.setProperty('--switchcolor', '#fccbf5');
+      document.documentElement.style.setProperty('--hovercolor', 'rgb(185, 71, 185)');
+      document.getElementById("menuIcon").src = "listSakura.png";
+  }
+  if (document.getElementsByClassName("arrow")[0].classList.contains("rotate-arrow-down") == false){
+      document.getElementsByClassName("arrow")[0].classList.remove("rotate-arrow-up");
+      void document.getElementsByClassName("arrow")[0].offsetWidth; //trigger reflow
+      document.getElementsByClassName("arrow")[0].classList.add("rotate-arrow-down");
+  }
+  localStorage.setItem("bg", value);
+  Theme_click_count++;
+}
+const theme_selector = document.querySelector('.theme-option');
+const theme_select = theme_selector.children[1]; //exclude children 0 which is the div arrow
+theme_select.value = localStorage.getItem('Theme-select-value');
+if(theme_select.value == ''){theme_select.value = 0;} //set value to 0 if empty localStorage
+theme_selector.addEventListener('mousedown', e => {
+  e.preventDefault();
+  Theme_click_count++;
+  const theme_select = theme_selector.children[1]; //exclude children 0 which is the div arrow
+  const dropDown = document.createElement('ul');
+  dropDown.className = 'dropdown-option';
+  var i = 0;
+  function CreateOption(){
+      setTimeout(function(){
+      const option = [...theme_select.children][i];
+      const dropDownOption = document.createElement('li');
+      dropDownOption.textContent = option.textContent;
+      if(i == 0){dropDownOption.style.color = "white"; dropDownOption.style.textShadow = "3px 3px 5px rgba(228, 228, 228, 0.973)";}
+      else if(i == 1){dropDownOption.style.color = "black"; dropDownOption.style.textShadow = "1px 1px 5px rgba(92, 88, 88, 0.973)";}
+      else if(i == 2){dropDownOption.style.color = "rgb(228, 103, 124)"; dropDownOption.style.textShadow = "1px 1px 5px rgba(241, 9, 199, 0.973)";}
+      dropDownOption.addEventListener('mousedown', e => {
+          e.stopPropagation();
+          if (option.value == 0) {ChangeTheme(0);}
+          else if (option.value == 1) {ChangeTheme(1);}
+          else if (option.value == 2) {ChangeTheme(2);}
+          theme_select.value = option.value;
+          localStorage.setItem('Theme-select-value', option.value);
+          dropDown.remove();
+      })
+      //dropdown animation
+      dropDown.appendChild(dropDownOption);
+      dropDownOption.classList.remove("open-option");
+      void dropDownOption.offsetWidth; //trigger reflow
+      dropDownOption.classList.add("open-option");
+      i++;
+      if(i < 3){CreateOption();}
+      }, 50);
+  }
+  CreateOption();
+  theme_selector.appendChild(dropDown);
+  //close dropdown when clicking outside
+  document.addEventListener('click', e => {
+      if((Theme_click_count % 2 == 0) || !theme_selector.contains(e.target)){
+          if(document.getElementsByClassName("arrow")[0].classList.contains("rotate-arrow-down") == false){
+              document.getElementsByClassName("arrow")[0].classList.remove("rotate-arrow-up");
+              void document.getElementsByClassName("arrow")[0].offsetWidth; //trigger reflow
+              document.getElementsByClassName("arrow")[0].classList.add("rotate-arrow-down");
+              Theme_click_count++;
+          }
+          dropDown.remove();
+      }
+  })
+  //arrow animation
+  if(document.getElementsByClassName("arrow")[0].classList.contains("rotate-arrow-up") == false){
+      document.getElementsByClassName("arrow")[0].classList.remove("rotate-arrow-down");
+      void document.getElementsByClassName("arrow")[0].offsetWidth; //trigger reflow
+      document.getElementsByClassName("arrow")[0].classList.add("rotate-arrow-up");
+  }
+  else if (document.getElementsByClassName("arrow")[0].classList.contains("rotate-arrow-down") == false){
+      document.getElementsByClassName("arrow")[0].classList.remove("rotate-arrow-up");
+      void document.getElementsByClassName("arrow")[0].offsetWidth; //trigger reflow
+      document.getElementsByClassName("arrow")[0].classList.add("rotate-arrow-down");
+  }
+})
+//open menu
+function openNav(){
+    document.getElementById("menu").style.display = "block";
+    document.getElementsByClassName("navbar")[0].classList.remove("close-navbar");
+    void document.getElementsByClassName("navbar")[0].offsetWidth; //trigger reflow
+    document.getElementsByClassName("navbar")[0].classList.add("open-navbar");
+}
+//close menu
+function closeNav(){
+    setTimeout(function() {document.getElementById("menu").style.display = "none";}, 300);
+    document.getElementsByClassName("navbar")[0].classList.remove("open-navbar");
+    void document.getElementsByClassName("navbar")[0].offsetWidth; //trigger reflow
+    document.getElementsByClassName("navbar")[0].classList.add("close-navbar");
+}
+document.addEventListener('click', e => {
+    if(document.querySelector("#menu").contains(e.target) && !document.querySelector(".navbar").contains(e.target)){
+        setTimeout(function() {document.getElementById("menu").style.display = "none";}, 300);
+        document.getElementsByClassName("navbar")[0].classList.remove("open-navbar");
+        void document.getElementsByClassName("navbar")[0].offsetWidth; //trigger reflow
+        document.getElementsByClassName("navbar")[0].classList.add("close-navbar");
+    }
+})
