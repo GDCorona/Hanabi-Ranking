03fc930368ec4dc594a5256e8863c6e0bdc1bc2d
__Hanabi__ <101558859+GDCorona@users.noreply.github.com>
2023-08-28 00:16:39 +0700

    Update demonlist.js

--- Demonlist/demonlist.js
+++ Demonlist/demonlist.js
@@ -1,124 +1,124 @@
+/*individual section (unique by pages)*/
 /*top1*/
 var modal1 = document.getElementById("top1Info");
 var btn1 = document.getElementById("t1");
-var span1 = document.getElementsByClassName("close1")[0];
+var span1 = document.getElementById("close1");
 btn1.onclick = function(){modal1.style.display = "block";}
 span1.onclick = function(){modal1.style.display = "none";}
 /*top1*/
 var modal2 = document.getElementById("top2Info");
 var btn2 = document.getElementById("t2");
-var span2 = document.getElementsByClassName("close2")[0];
+var span2 = document.getElementById("close2");
 btn2.onclick = function(){modal2.style.display = "block";}
 span2.onclick = function(){modal2.style.display = "none";}
 /*top3*/
 var modal3 = document.getElementById("top3Info");
 var btn3 = document.getElementById("t3");
-var span3 = document.getElementsByClassName("close3")[0];
+var span3 = document.getElementById("close3");
 btn3.onclick = function(){modal3.style.display = "block";}
 span3.onclick = function(){modal3.style.display = "none";}
 /*top4*/
 var modal4 = document.getElementById("top4Info");
 var btn4 = document.getElementById("t4");
-var span4 = document.getElementsByClassName("close4")[0];
+var span4 = document.getElementById("close4");
 btn4.onclick = function(){modal4.style.display = "block";}
 span4.onclick = function(){modal4.style.display = "none";}
 /*top5*/
 var modal5 = document.getElementById("top5Info");
 var btn5 = document.getElementById("t5");
-var span5 = document.getElementsByClassName("close5")[0];
+var span5 = document.getElementById("close5");
 btn5.onclick = function(){modal5.style.display = "block";}
 span5.onclick = function(){modal5.style.display = "none";}
 /*top6*/
 var modal6 = document.getElementById("top6Info");
 var btn6 = document.getElementById("t6");
-var span6 = document.getElementsByClassName("close6")[0];
+var span6 = document.getElementById("close6");
 btn6.onclick = function(){modal6.style.display = "block";}
 span6.onclick = function(){modal6.style.display = "none";}
 /*top7*/
 var modal7 = document.getElementById("top7Info");
 var btn7 = document.getElementById("t7");
-var span7 = document.getElementsByClassName("close7")[0];
+var span7 = document.getElementById("close7");
 btn7.onclick = function(){modal7.style.display = "block";}
 span7.onclick = function(){modal7.style.display = "none";}
 /*top8*/
 var modal8 = document.getElementById("top8Info");
 var btn8 = document.getElementById("t8");
-var span8 = document.getElementsByClassName("close8")[0];
+var span8 = document.getElementById("close8");
 btn8.onclick = function(){modal8.style.display = "block";}
 span8.onclick = function(){modal8.style.display = "none";}
 /*top9*/
 var modal9 = document.getElementById("top9Info");
 var btn9 = document.getElementById("t9");
-var span9 = document.getElementsByClassName("close9")[0];
+var span9 = document.getElementById("close9");
 btn9.onclick = function(){modal9.style.display = "block";}
 span9.onclick = function(){modal9.style.display = "none";}
 /*top10*/
 var modal10 = document.getElementById("top10Info");
 var btn10 = document.getElementById("t10");
-var span10 = document.getElementsByClassName("close10")[0];
+var span10 = document.getElementById("close10");
 btn10.onclick = function(){modal10.style.display = "block";}
 span10.onclick = function(){modal10.style.display = "none";}
 /*top11*/
 var modal11 = document.getElementById("top11Info");
 var btn11 = document.getElementById("t11");
-var span11 = document.getElementsByClassName("close11")[0];
+var span11 = document.getElementById("close11");
 btn11.onclick = function(){modal11.style.display = "block";}
 span11.onclick = function(){modal11.style.display = "none";}
 /*top12*/
 var modal12 = document.getElementById("top12Info");
 var btn12 = document.getElementById("t12");
-var span12 = document.getElementsByClassName("close12")[0];
+var span12 = document.getElementById("close12");
 btn12.onclick = function(){modal12.style.display = "block";}
 span12.onclick = function(){modal12.style.display = "none";}
 /*top13*/
 var modal13 = document.getElementById("top13Info");
 var btn13 = document.getElementById("t13");
-var span13 = document.getElementsByClassName("close13")[0];
+var span13 = document.getElementById("close13");
 btn13.onclick = function(){modal13.style.display = "block";}
 span13.onclick = function(){modal13.style.display = "none";}
 /*top14*/
 var modal14 = document.getElementById("top14Info");
 var btn14 = document.getElementById("t14");
-var span14 = document.getElementsByClassName("close14")[0];
+var span14 = document.getElementById("close14");
 btn14.onclick = function(){modal14.style.display = "block";}
 span14.onclick = function(){modal14.style.display = "none";}
 /*top15*/
 var modal15 = document.getElementById("top15Info");
 var btn15 = document.getElementById("t15");
-var span15 = document.getElementsByClassName("close15")[0];
+var span15 = document.getElementById("close15");
 btn15.onclick = function(){modal15.style.display = "block";}
 span15.onclick = function(){modal15.style.display = "none";}
 /*top16*/
 var modal16 = document.getElementById("top16Info");
 var btn16 = document.getElementById("t16");
-var span16 = document.getElementsByClassName("close16")[0];
+var span16 = document.getElementById("close16");
 btn16.onclick = function(){modal16.style.display = "block";}
 span16.onclick = function(){modal16.style.display = "none";}
 /*top17*/
 var modal17 = document.getElementById("top17Info");
 var btn17 = document.getElementById("t17");
-var span17 = document.getElementsByClassName("close17")[0];
+var span17 = document.getElementById("close17");
 btn17.onclick = function(){modal17.style.display = "block";}
 span17.onclick = function(){modal17.style.display = "none";}
 /*top18*/
 var modal18 = document.getElementById("top18Info");
 var btn18 = document.getElementById("t18");
-var span18 = document.getElementsByClassName("close18")[0];
+var span18 = document.getElementById("close18");
 btn18.onclick = function(){modal18.style.display = "block";}
 span18.onclick = function(){modal18.style.display = "none";}
 /*top19*/
 var modal19 = document.getElementById("top19Info");
 var btn19 = document.getElementById("t19");
-var span19 = document.getElementsByClassName("close19")[0];
+var span19 = document.getElementById("close19");
 btn19.onclick = function(){modal19.style.display = "block";}
 span19.onclick = function(){modal19.style.display = "none";}
 /*top20*/
 var modal20 = document.getElementById("top20Info");
 var btn20 = document.getElementById("t20");
-var span20 = document.getElementsByClassName("close20")[0];
+var span20 = document.getElementById("close20");
 btn20.onclick = function(){modal20.style.display = "block";}
 span20.onclick = function(){modal20.style.display = "none";}
-
 /*close when clicking outside the modal*/
 window.onclick = function(event) {
 	if (event.target == modal1) {modal1.style.display = "none";}
@@ -143,6 +143,28 @@ window.onclick = function(event) {
 	else if (event.target == modal20) {modal20.style.display = "none";}
 }
 /*shared section (for every pages)*/
+//open menu
+function openNav(){
+  document.getElementById("menu").style.display = "block";
+  document.getElementsByClassName("navbar")[0].classList.remove("close-navbar");
+  void document.getElementsByClassName("navbar")[0].offsetWidth; //trigger reflow
+  document.getElementsByClassName("navbar")[0].classList.add("open-navbar");
+}
+//close menu
+function closeNav(){
+  setTimeout(function() {document.getElementById("menu").style.display = "none";}, 300);
+  document.getElementsByClassName("navbar")[0].classList.remove("open-navbar");
+  void document.getElementsByClassName("navbar")[0].offsetWidth; //trigger reflow
+  document.getElementsByClassName("navbar")[0].classList.add("close-navbar");
+}
+document.addEventListener('click', e => {
+  if(document.querySelector("#menu").contains(e.target) && !document.querySelector(".navbar").contains(e.target)){
+      setTimeout(function() {document.getElementById("menu").style.display = "none";}, 300);
+      document.getElementsByClassName("navbar")[0].classList.remove("open-navbar");
+      void document.getElementsByClassName("navbar")[0].offsetWidth; //trigger reflow
+      document.getElementsByClassName("navbar")[0].classList.add("close-navbar");
+  }
+})
 //jump to top
 let mybutton = document.getElementById("jump-to-top");
 window.onscroll = function() {scrollFunction()};
@@ -169,7 +191,7 @@ if(loadBG == 0){
   document.documentElement.style.setProperty('--color4', 'aliceblue');
   document.documentElement.style.setProperty('--switchcolor', '#cefaf8');
   document.documentElement.style.setProperty('--hovercolor', 'rgb(70, 157, 228)');
-  document.getElementById("menuIcon").src = "listLight.png";
+  document.getElementById("menuIcon").src = "./Photos/listLight.png";
 }
 else if (loadBG == 1){
   document.getElementsByTagName("body")[0].style.backgroundImage = "none";
@@ -180,7 +202,7 @@ else if (loadBG == 1){
   document.documentElement.style.setProperty('--color4', 'rgba(135, 167, 172, 0.925)');
   document.documentElement.style.setProperty('--switchcolor', '#8d8d8d');
   document.documentElement.style.setProperty('--hovercolor', 'rgb(220, 221, 243)');
-  document.getElementById("menuIcon").src = "listDark.png";
+  document.getElementById("menuIcon").src = "./Photos/listDark.png";
 }
 else if (loadBG == 2){
   document.getElementsByTagName("body")[0].style.backgroundImage = "none";
@@ -191,7 +213,7 @@ else if (loadBG == 2){
   document.documentElement.style.setProperty('--color4', 'rgba(255, 232, 253, 0.8)');
   document.documentElement.style.setProperty('--switchcolor', '#fccbf5');
   document.documentElement.style.setProperty('--hovercolor', 'rgb(185, 71, 185)');
-  document.getElementById("menuIcon").src = "listSakura.png";
+  document.getElementById("menuIcon").src = "./Photos/listSakura.png";
 }
 var Theme_click_count = 0;
 function ChangeTheme(value){
@@ -204,7 +226,8 @@ function ChangeTheme(value){
       document.documentElement.style.setProperty('--color4', 'aliceblue');
       document.documentElement.style.setProperty('--switchcolor', '#cefaf8');
       document.documentElement.style.setProperty('--hovercolor', 'rgb(70, 157, 228)');
-      document.getElementById("menuIcon").src = "listLight.png";
+      document.getElementById("menuIcon").src = "./Photos/listLight.png";
+      localStorage.setItem('background', -1);
   }
   else if (value == 1){
       document.getElementsByTagName("body")[0].style.backgroundImage = "none";
@@ -215,7 +238,8 @@ function ChangeTheme(value){
       document.documentElement.style.setProperty('--color4', 'rgba(135, 167, 172, 0.925)');
       document.documentElement.style.setProperty('--switchcolor', '#8d8d8d');
       document.documentElement.style.setProperty('--hovercolor', 'rgb(220, 221, 243)');
-      document.getElementById("menuIcon").src = "listDark.png";
+      document.getElementById("menuIcon").src = "./Photos/listDark.png";
+      localStorage.setItem('background', -1);
   }
   else if (value == 2){
       document.getElementsByTagName("body")[0].style.backgroundImage = "none";
@@ -226,7 +250,8 @@ function ChangeTheme(value){
       document.documentElement.style.setProperty('--color4', 'rgba(255, 232, 253, 0.8)');
       document.documentElement.style.setProperty('--switchcolor', '#fccbf5');
       document.documentElement.style.setProperty('--hovercolor', 'rgb(185, 71, 185)');
-      document.getElementById("menuIcon").src = "listSakura.png";
+      document.getElementById("menuIcon").src = "./Photos/listSakura.png";
+      localStorage.setItem('background', -1);
   }
   if (document.getElementsByClassName("arrow")[0].classList.contains("rotate-arrow-down") == false){
       document.getElementsByClassName("arrow")[0].classList.remove("rotate-arrow-up");
@@ -299,25 +324,60 @@ theme_selector.addEventListener('mousedown', e => {
       document.getElementsByClassName("arrow")[0].classList.add("rotate-arrow-down");
   }
 })
-//open menu
-function openNav(){
-    document.getElementById("menu").style.display = "block";
-    document.getElementsByClassName("navbar")[0].classList.remove("close-navbar");
-    void document.getElementsByClassName("navbar")[0].offsetWidth; //trigger reflow
-    document.getElementsByClassName("navbar")[0].classList.add("open-navbar");
-}
-//close menu
-function closeNav(){
-    setTimeout(function() {document.getElementById("menu").style.display = "none";}, 300);
-    document.getElementsByClassName("navbar")[0].classList.remove("open-navbar");
-    void document.getElementsByClassName("navbar")[0].offsetWidth; //trigger reflow
-    document.getElementsByClassName("navbar")[0].classList.add("close-navbar");
-}
-document.addEventListener('click', e => {
-    if(document.querySelector("#menu").contains(e.target) && !document.querySelector(".navbar").contains(e.target)){
-        setTimeout(function() {document.getElementById("menu").style.display = "none";}, 300);
-        document.getElementsByClassName("navbar")[0].classList.remove("open-navbar");
-        void document.getElementsByClassName("navbar")[0].offsetWidth; //trigger reflow
-        document.getElementsByClassName("navbar")[0].classList.add("close-navbar");
+/*secret*/
+var currentbg = -1;
+var currentSounds = -1;
+var check = true;
+var body = document.getElementsByTagName("body")[0];
+var secretsounds = document.getElementById("secretSounds");
+var bg = 
+[
+    "url('https://i.pinimg.com/originals/52/a2/62/52a262d0944bcf487bf6c323c7c64c67.jpg')",
+    "url('https://i.pinimg.com/originals/28/61/fe/2861fe7c95b8124208fecdf63f818572.jpg')",
+    "url('https://i.pinimg.com/originals/5f/32/73/5f32731ab2f9e18ce9509913c05eac4b.jpg')",
+    "url('https://i.pinimg.com/originals/bb/37/a6/bb37a6037cce5fc895a8450914a07458.jpg')",
+    "url('https://i.pinimg.com/originals/b3/a7/99/b3a79923cb5dc7add78054d5d005801a.jpg')",
+    "url('https://i.pinimg.com/originals/26/59/93/2659934c11ee4ca9305de593b8c1e401.jpg')",
+    "url('https://i.pinimg.com/originals/e9/54/d1/e954d16d765536dd99186667953a5f66.jpg')",
+    "url('https://i.pinimg.com/originals/5b/c8/ae/5bc8aeb83131e38bbab8ba96fc4959d8.jpg')",
+    "url('https://i.pinimg.com/originals/7c/26/c8/7c26c8d4fe0b8b6f4ce595a55768488f.jpg')",
+    "url('https://i.pinimg.com/originals/40/6b/5d/406b5d7bcae6ebdfacd5ce20d7c3a823.jpg')"
+];
+var sounds = 
+[
+    "https://feeds.soundcloud.com/stream/1493606947-corona-689894639-kaguya-noise-2.mp3",
+    "https://feeds.soundcloud.com/stream/1493606929-corona-689894639-chika-noise-2.mp3",
+    "https://feeds.soundcloud.com/stream/1493606920-corona-689894639-chika-noise-1.mp3",
+    "https://feeds.soundcloud.com/stream/1493606914-corona-689894639-rikka-noise-2.mp3",
+    "https://feeds.soundcloud.com/stream/1602173829-corona-689894639-jibril-voice.mp3"
+];
+var background = parseInt(localStorage.getItem('background'));
+if(background == -1) {body.style.backgroundImage = "none";}
+else{body.style.backgroundImage = bg[background];}
+function runSecret(){
+    if(check) {
+        check = false;
+        randombg(); 
+        randomSounds(); 
+        setTimeout(function(){check = true;}, 3000);
     }
-})
+    else {alert("Too fast!");}
+}
+function randombg(){
+    var random = Math.floor(Math.random() * 10);
+    if(currentbg == random){randombg();}
+    else{
+        currentbg = random;
+        body.style.backgroundImage = bg[currentbg];
+        localStorage.setItem('background', currentbg);
+    }
+}
+function randomSounds(){
+    var random = Math.floor(Math.random() * 5);
+    if(currentSounds == random){randomSounds();}
+    else{
+        currentSounds = random;
+        secretsounds.src = sounds[currentSounds];
+        secretsounds.play();
+    }
+}
