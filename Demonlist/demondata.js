c54e0f749fb99942fef1eb20c92f33b2b60ffda2
__Hanabi__ <101558859+GDCorona@users.noreply.github.com>
2025-07-21 11:17:26 +0700

    Add files via upload

---
+++ Demonlist/demondata.js
@@ -0,0 +1,222 @@
+export const demonList = [
+  {
+    name: "RANDOM ACCESS MEMORY",
+    image: "./Thumbnails/RAM.png",
+    difficulty: "Extreme Demon",
+    fps: "300",
+    attempts: "22320",
+    worstFail: "98%",
+    enjoyment: "9.5/10",
+    date: "11/6/2025",
+    video: "https://youtu.be/elq1hwBwEDw"
+  },
+  {
+    name: "Super Probably Level",
+    image: "./Thumbnails/SPL.png",
+    difficulty: "Extreme Demon",
+    fps: "240",
+    attempts: "27027",
+    worstFail: "93%x3",
+    enjoyment: "7.5/10",
+    date: "31/3/2025",
+    video: "https://youtu.be/qFXK0fHsJ88"
+  },
+  {
+    name: "Retention Rush",
+    image: "./Thumbnails/Retention Rush.png",
+    difficulty: "Extreme Demon",
+    fps: "165",
+    attempts: "22086",
+    worstFail: "92%",
+    enjoyment: "8.7/10",
+    date: "21/12/2024",
+    video: "https://youtu.be/wBUWu2BWzLA"
+  },
+  {
+    name: "Killbot",
+    image: "./Thumbnails/Killbot.png",
+    difficulty: "Extreme Demon",
+    fps: "165",
+    attempts: "19249",
+    worstFail: "97% x3",
+    enjoyment: "9/10",
+    date: "27/1/2025",
+    video: "https://youtu.be/gg5swSNhVUE"
+  },
+  {
+    name: "The Mainframe",
+    image: "./Thumbnails/The Mainframe.png",
+    difficulty: "Extreme Demon",
+    fps: "60",
+    attempts: "35983",
+    worstFail: "75%",
+    enjoyment: "7.5/10",
+    date: "5/11/2024",
+    video: "https://youtu.be/16SSkm92iQ8"
+  },
+  {
+    name: "Down Bass",
+    image: "./Thumbnails/Down Bass.png",
+    difficulty: "Extreme Demon",
+    fps: "240",
+    attempts: "4902",
+    worstFail: "69%",
+    enjoyment: "7.7/10",
+    date: "12/7/2025",
+    video: "https://youtu.be/Vb0XC4p-Tpg"
+  },
+  {
+    name: "EnvY",
+    image: "./Thumbnails/Envy.png",
+    difficulty: "Extreme Demon",
+    fps: "360",
+    attempts: "~8508",
+    worstFail: "84%",
+    enjoyment: "9/10",
+    date: "29/6/2025",
+    video: "https://youtu.be/PrXQQB5fs4A"
+  },
+  {
+    name: "A Bizarre Phantasm",
+    image: "./Thumbnails/ABP.png",
+    difficulty: "Extreme Demon",
+    fps: "165",
+    attempts: "13266",
+    worstFail: "81%",
+    enjoyment: "3/10",
+    date: "30/12/2024",
+    video: "https://youtu.be/2H_QSVao7DI"
+  },
+  {
+    name: "Moment",
+    image: "./Thumbnails/Moment.png",
+    difficulty: "Extreme Demon",
+    fps: "144",
+    attempts: "~19112",
+    worstFail: "87%",
+    enjoyment: "8.7/10",
+    date: "13/9/2024",
+    video: "https://youtu.be/sFdpuZMi7QY"
+  },
+  {
+    name: "Coolie Mazie Paloozi",
+    image: "./Thumbnails/Coolie Mazie Paloozi.jpg",
+    difficulty: "Extreme Demon",
+    fps: "144",
+    attempts: "18347",
+    worstFail: "98% x2",
+    enjoyment: "10/10",
+    date: "8/2/2024",
+    video: "https://youtu.be/tF7TG_kGP7g"
+  },
+  {
+    name: "Maybe Possibly Thing",
+    image: "./Thumbnails/MPT.png",
+    difficulty: "Extreme Demon",
+    fps: "288",
+    attempts: "4686",
+    worstFail: "86%",
+    enjoyment: "6.7/10",
+    date: "23/4/2025",
+    video: "https://youtu.be/9G9Q7jrEgnM"
+  },
+  {
+    name: "Deception Dive",
+    image: "./Thumbnails/Deception Dive.jpg",
+    difficulty: "Extreme Demon",
+    fps: "144",
+    attempts: "~11k",
+    worstFail: "82%",
+    enjoyment: "7/10",
+    date: "20/5/2023",
+    video: "https://youtu.be/h_NeAjalj8A"
+  },
+  {
+    name: "Falling Up",
+    image: "./Thumbnails/Falling Up.png",
+    difficulty: "Extreme Demon",
+    fps: "120",
+    attempts: "~13k",
+    worstFail: "98%",
+    enjoyment: "9/10",
+    date: "23/11/2023",
+    video: "https://youtu.be/opMqgqghVx8"
+  },
+  {
+    name: "Cluttered Confusion",
+    image: "./Thumbnails/Cluttered Confusion.png",
+    difficulty: "Extreme Demon",
+    fps: "60",
+    attempts: "19880",
+    worstFail: "93%",
+    enjoyment: "8/10",
+    date: "15/7/2024",
+    video: "https://youtu.be/2UKSeSTGWVM"
+  },
+  {
+    name: "MELTDOWN",
+    image: "./Thumbnails/Meltdown.jpg",
+    difficulty: "Extreme Demon",
+    fps: "144",
+    attempts: "~8k",
+    worstFail: "96%",
+    enjoyment: "8/10",
+    date: "26/1/2023",
+    video: "https://youtu.be/BbGiPoHRufo"
+  },
+  {
+    name: "Triple Six",
+    image: "./Thumbnails/Triple Six.png",
+    difficulty: "Extreme Demon",
+    fps: "60",
+    attempts: "17218",
+    worstFail: "95% x3",
+    enjoyment: "7.5/10",
+    date: "1/4/2024",
+    video: "https://youtu.be/6qLow23R3AE"
+  },
+  {
+    name: "Night Party",
+    image: "./Thumbnails/Night Party.png",
+    difficulty: "Extreme Demon",
+    fps: "144",
+    attempts: "8577",
+    worstFail: "81% x2",
+    enjoyment: "4/10",
+    date: "29/1/2024",
+    video: "https://youtu.be/YIaHcJRDKRQ"
+  },
+  {
+    name: "Novalis",
+    image: "./Thumbnails/Novalis.jpg",
+    difficulty: "Extreme Demon",
+    fps: "75",
+    attempts: "15078",
+    worstFail: "93%",
+    enjoyment: "9/10",
+    date: "10/9/2022",
+    video: "https://youtu.be/ejUUtMAdgrY"
+  },
+  {
+    name: "HURRICANE",
+    image: "./Thumbnails/HURRICANE.png",
+    difficulty: "Extreme Demon",
+    fps: "144",
+    attempts: "6217",
+    worstFail: "95%",
+    enjoyment: "9.5/10",
+    date: "7/7/2024",
+    video: "https://youtu.be/e5ByGqvrmQg"
+  },
+  {
+    name: "Allegiance",
+    image: "./Thumbnails/Allegiance.jpg",
+    difficulty: "Extreme Demon",
+    fps: "144",
+    attempts: "5254",
+    worstFail: "94%",
+    enjoyment: "7.7/10",
+    date: "11/7/2023",
+    video: "https://youtu.be/Fd7uKTwwZrc"
+  }
+];
