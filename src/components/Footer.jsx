c1556403b5589e612745abb8818ec724ebd8418e
GDCorona <phanthethanhson@gmail.com>
2026-05-05 17:11:59 +0700

    small update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect, useRef } from 'react';
 import { useLocation } from 'react-router-dom';
 import { CONFIG } from '../config';
 import { useDialog } from '../context/DialogContext';
-let globalIsFetching = false;
+
 export default function Footer({ onNavigate }) {
     const location = useLocation();
     const [visits, setVisits] = useState("...");
@@ -47,12 +47,12 @@ export default function Footer({ onNavigate }) {
     }, [isDemonInfoPage]);
 
     const handleSupportClick = async () => {
-        if (globalIsFetching) return;
-        if (localStorage.getItem("supportClicked")) {
+        if (isFetchingRef.current || localStorage.getItem("supportClicked")) return;
+        if (!isFetchingRef.current && localStorage.getItem("supportClicked")) {
             showDialog("You have already clicked!", { showMeme: false});
             return;
         }
-        globalIsFetching = true;
+        isFetchingRef.current = true;
         try {
             const res = await fetch(`${CONFIG.API_BASE_URL}/api/visits`, { method: "POST" });
             const data = await res.json();
@@ -71,7 +71,7 @@ export default function Footer({ onNavigate }) {
         } catch (err) {
             console.error("Failed to update visits:", err);
         } finally {
-            globalIsFetching = false;
+            isFetchingRef.current = false;
         }
     };
     const handleLinkClick = (e, path) => {
